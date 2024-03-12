85c2fef931588f1448b3c347df06f18bf7a99ca2
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-03-12 10:49:02 +0700

    menambahkan implementasi dengan state-based construction

---
+++ tpmodul4_1302223007/DoorMachine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace tpmodul4_1302223007
+{
+	public enum DoorState
+	{
+		Terkunci, Terbuka
+	}
+
+	public enum Trigger
+	{
+		KunciPintu, BukaPintu
+	}
+
+	public class DoorMachine
+	{
+		public class Transition
+		{
+			public DoorState stateAwal;
+			public DoorState stateAkhir;
+			public Trigger trigger;
+
+			public Transition(DoorState stateAwal, DoorState stateAkhir, Trigger trigger)
+			{
+				this.stateAwal = stateAwal;
+				this.stateAkhir = stateAkhir;
+				this.trigger = trigger;
+			}
+		}
+
+        Transition[] transisi =
+            {
+                new Transition(DoorState.Terkunci, DoorState.Terbuka, Trigger.BukaPintu),
+                new Transition(DoorState.Terbuka, DoorState.Terkunci, Trigger.KunciPintu),
+            };
+
+        public DoorState currentState = DoorState.Terkunci;
+
+        public DoorState GetNextState(DoorState stateAwal, Trigger trigger)
+        {
+            DoorState stateAkhir = stateAwal;
+
+            for (int i = 0; i < transisi.Length; i++)
+            {
+                Transition perubahan = transisi[i];
+
+                if (stateAwal == perubahan.stateAwal && trigger == perubahan.trigger)
+                {
+                    stateAkhir = perubahan.stateAkhir;
+                }
+            }
+
+            return stateAkhir;
+        }
+
+		public void ActivateTrigger(Trigger trigger)
+		{
+			currentState = GetNextState(currentState, trigger);
+
+			Console.WriteLine(currentState == DoorState.Terkunci ? "Pintu terkunci" : "Pintu tidak terkunci");
+		}
+    }
+}
+
--- tpmodul4_1302223007/Program.cs
+++ tpmodul4_1302223007/Program.cs
@@ -5,6 +5,36 @@ internal class Program
     private static void Main(string[] args)
     {
         KodePos kodePos = new KodePos();
-        Console.WriteLine(KodePos.Kelurahan.Batununggal + " - " + kodePos.getKodePos(KodePos.Kelurahan.Cijaura));
+
+        void printInfo(KodePos.Kelurahan kelurahan)
+        {
+            Console.WriteLine("- " + kelurahan + " (" + kodePos.getKodePos(kelurahan) + ")");
+        }
+
+        Console.WriteLine("KELURAHAN (KODE POS)");
+        printInfo(KodePos.Kelurahan.Batununggal);
+        printInfo(KodePos.Kelurahan.Kujangsari);
+        printInfo(KodePos.Kelurahan.Mengger);
+        printInfo(KodePos.Kelurahan.Wates);
+        printInfo(KodePos.Kelurahan.Cijaura);
+        printInfo(KodePos.Kelurahan.Jatisari);
+        printInfo(KodePos.Kelurahan.Margasari);
+        printInfo(KodePos.Kelurahan.Sekejati);
+        printInfo(KodePos.Kelurahan.Kebonwaru);
+        printInfo(KodePos.Kelurahan.Maleer);
+        printInfo(KodePos.Kelurahan.Samoja);
+
+        Console.WriteLine("");
+
+        DoorMachine door = new DoorMachine();
+        Console.WriteLine("State awal : " + door.currentState);
+        Console.WriteLine("---trigger kunci pintu---");
+        door.ActivateTrigger(Trigger.KunciPintu);
+        Console.WriteLine("---trigger buka pintu---");
+        door.ActivateTrigger(Trigger.BukaPintu);
+        Console.WriteLine("---trigger buka pintu---");
+        door.ActivateTrigger(Trigger.BukaPintu);
+        Console.WriteLine("---trigger kunci pintu---");
+        door.ActivateTrigger(Trigger.KunciPintu);
     }
 }
